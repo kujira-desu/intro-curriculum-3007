ac96eb80817194c66c9e4e96f27f96b130c4802d
kujira_desu <test@test.hoge.hoge>
2020-03-08 22:25:22 +0900

    isMultipleOfSeventeen関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
+/**
+ * 17の倍数が渡された場合のみtrueを返す
+ * @param {value} 数値
+ * @resurt {boolean} 17の倍数ならtrue
+ */
+function isMultipleOfSeventeen(value) {
+    return value % 17 === 0;
+}
+
+module.exports = { isMultipleOfSeventeen
 }
